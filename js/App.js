71a2e2091f3bec3b20d84e5665aa8dc4fcc60bdd
stanislav <shilov@scioffice.com>
2023-01-03 01:05:00 +0700

    fix drag'n drop

--- js/App.js
+++ js/App.js
@@ -1,10 +1,16 @@
 import PostService from "./API/PostService.js";
 import { useFetching } from "./hooks/useFetching.js";
-import render from "./FunctionsForDOM/Render.js";
+import render from "./FunctionsForDOM/render.js";
 import store from "./store/store.js";
-import dragAndDrop from "./FunctionsForDOM/DragAndDrop.js";
+import dragAndDrop from "./FunctionsForDOM/dragAndDrop.js";
+import { removeItems } from "./FunctionsForDOM/removeItems.js";
 
 const App = () => {
+	removeItems(document.getElementsByClassName("main__list")[0]);
+	const limitVar = document.getElementsByClassName("header__top-input")[0].value;
+	if (limitVar >= 0) {
+		store.limit = limitVar;
+	}
 	const fetchUsers = useFetching(async () => {
 		const responce = await PostService.getUsers(store.limit);
 		const data = await responce.json();
