3f0f172f6111b7a4d0080ebf058494b3fbc51769
stanislav <shilov@scioffice.com>
2023-01-04 13:16:10 +0700

    added feature sorted and  download new quantity el

--- js/App.js
+++ js/App.js
@@ -1,27 +1,70 @@
+import store from "./store/store.js";
 import PostService from "./API/PostService.js";
 import { useFetching } from "./hooks/useFetching.js";
-import render from "./FunctionsForDOM/render.js";
-import store from "./store/store.js";
-import dragAndDrop from "./FunctionsForDOM/dragAndDrop.js";
-import { removeItems } from "./FunctionsForDOM/removeItems.js";
+import { render } from "./utils/render.js";
+import { dragAndDrop } from "./utils/dragAndDrop.js";
+import { removeItems } from "./utils/removeItems.js";
+import { sortedProducts } from "./utils/sortedProducts.js";
 
 const App = () => {
-	removeItems(document.getElementsByClassName("main__list")[0]);
-	const limitVar = document.getElementsByClassName("header__top-input")[0].value;
-	if (limitVar >= 0) {
-		store.limit = limitVar;
-	}
+	// Получение html-элементов из DOM-дерева
+	const mainList = document.querySelector(".main__list"); // список самих элементов
+	const btn = document.querySelector(".header__bot-btn"); // кнопка для рендеринга новых элементов
+	const select = document.querySelector("#header__drop-select"); // select для выбора сортировки элементов
+
+	/*Использование хука useFetching для загрузки данных из fakeAPI
+	Для оптимизации я использую всего одну загрузку данных с сервера, т.к. заранее известен размер массива данных, при этом размер не сильно большой(30) и не будет изменяться со временем */
 	const fetchUsers = useFetching(async () => {
-		const responce = await PostService.getUsers(store.limit);
+		// Запрос данных с помощью метода класса PostService
+		const responce = await PostService.getProducts();
 		const data = await responce.json();
+		// Помещаю данные в стор
 		store.products = data.products;
-		store.products.map(async (el, index) => {
-			render(el);
+		store.maxLimit = data.limit;
+		// Отрисовываю элементы списка в функции render
+		store.products.forEach(async (el, index) => {
+			index < store.limit && render(el, mainList);
 		});
 	});
 
+	// Чтобы отображать новое количество элементов в списке, которое указал пользователь, на кнопку навешиваю слушатель события по клику
+	btn.addEventListener("click", (event) => {
+		select.value = "default"; // Меняю элемент select на значение по умолчанию, нужно если пользователь сортировал элементы, загрузил новые и хочет снова отсортировать по тому же значению, иначе событие onChange для select не сработает
+		event.preventDefault(); // предотвращаю перезагрузку страницы
+		removeItems(mainList); // удаляю все элементы из списка, для новой отрисовки
+		const numberOfElements = document.getElementsByClassName("header__bot-input")[0].value;
+		// Проверка на валидное значение запрашиваемых элементов для списка
+		if (numberOfElements >= 0 && numberOfElements <= store.maxLimit) {
+			store.limit = numberOfElements;
+		} else {
+			alert("Введите корректное значение элементов: от 0 до 30");
+			return false;
+		}
+		// Отрисовка списка
+		store.products.forEach((el, index) => {
+			index < store.limit && render(el, mainList);
+		});
+	});
+
+	// Срабатывает при изменении вида сортировки
+	select.onchange = (event) => {
+		removeItems(mainList); // удаляю элементы из списка
+		let state = []; // создаю новый массив, чтобы не изменять данные в stor
+		for (let i = 0; i < store.limit; i++) {
+			// заполняю новый массив ровно стольким кол-вом элементов, сколько сейчас находиться на странице
+			state.push(store.products[i]);
+		}
+		// Функция sortedProducts возвращает отсортированный массив
+		state = sortedProducts(state, event.target.value);
+		// Отрисовка отсортированого массива
+		state.forEach((el) => {
+			render(el, mainList);
+		});
+	};
+
+	// Вызываю хук useFetching и функцию для функциональности Drag'n'Drop
 	fetchUsers();
-	dragAndDrop();
+	dragAndDrop(mainList);
 };
 
 export default App;
