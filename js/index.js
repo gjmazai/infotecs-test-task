458829e73dbe42fc8249165abe0ef890630da6cb
stanislav <shilov@scioffice.com>
2023-01-04 13:17:34 +0700

    removed unnecessary logic

--- js/index.js
+++ js/index.js
@@ -2,17 +2,12 @@ import App from "./App.js";
 
 const MAIN = "MAIN";
 
+// Инициализация приложения
 function InitialApp() {
 	App();
 }
-
+// Необходимо для первого рендеринга страницы с 10-ю элементами по умолчанию
 if (MAIN === "MAIN") {
 	InitialApp();
 }
-
-const btn = document.getElementsByClassName("header__top-btn")[0];
-btn.addEventListener("click", (evt) => {
-	evt.preventDefault();
-	InitialApp();
-});
 
